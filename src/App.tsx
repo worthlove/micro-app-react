7bcdd544bcee42f38790cb1a806de501ad4fcb60
GZF <13038085710g@gmail.com>
2024-06-21 21:01:31 +0800

    vscode设置头部文件信息

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+/*
+ * @Author: GZF
+ * @Date: 2024-06-16 15:54:48
+ * @Description: 设置文件头部函数说明，
+ *               快捷键：window：ctrl+win+t,mac：ctrl+cmd+t,linux: ctrl+meta+t, Ubuntu: ctrl+super+t
+ */
+
 import './App.css'
 
 function App() {
